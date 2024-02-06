fa7a36f41ffef5caf1f8d7d8bcd69ddbac10b6fd
SAGR2003 <sagr20032006@gmail.com>
2024-02-06 00:02:50 -0500

    Add spring structure

---
+++ src/main/java/co/edu/unisabana/sigaplus/SigaplusApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.sigaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unisabana/sigaplus/SigaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.sigaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
