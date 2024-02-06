72c7a8af082b638b89ea8b5b7acebbbc92161d85
SAGR2003 <sagr20032006@gmail.com>
2024-02-06 00:59:01 -0500

    Add student controller

---
+++ src/main/java/co/edu/unisabana/sigaplus/controller/StudentController.java
@@ -0,0 +1,37 @@
+package co.edu.unisabana.sigaplus.controller;
+
+import co.edu.unisabana.sigaplus.controller.dto.StudentResponseDTO;
+import co.edu.unisabana.sigaplus.model.Student;
+import co.edu.unisabana.sigaplus.service.IStudent;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/students")
+@AllArgsConstructor
+public class StudentController {
+    private IStudent studentService;
+
+    @GetMapping("/{id}")
+    private StudentResponseDTO getStudentById(@PathVariable int id){
+        Student student = studentService.getStudentById(id);
+
+        return new StudentResponseDTO(
+                student.getId(),
+                student.getName(),
+                student.getSemester()
+        );
+    }
+
+    @PostMapping("")
+    private StudentResponseDTO createStudent(@RequestBody Student student){
+        Student newStudent = studentService.saveStudent(student);
+
+        return new StudentResponseDTO(
+                newStudent.getId(),
+                newStudent.getName(),
+                newStudent.getSemester()
+        );
+    }
+
+}
